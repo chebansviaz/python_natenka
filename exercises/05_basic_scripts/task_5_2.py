9f22f2cf6b383997fd5217dd07fb3975ad2c681c
Cheban Andrey Mihailovich <acheban@mtb.ua>
2021-03-25 01:26:35 +0200

    5.2 done

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -24,3 +24,28 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+network = input('Введите IP-сети в формате: 10.1.1.0/24 : ')
+ip, mask = network.split("/")
+network_template = '''
+Network:
+{ip0:<8}  {ip1:<8}  {ip2:<8}  {ip3:<8}
+{ip0:08b}  {ip1:08b}  {ip2:08b}  {ip3:08b}
+'''
+mask_bin = "1"*int(mask) + "0"*(32-int(mask))
+mask_template = '''
+Mask:
+/{mask}
+{okt0:<8}  {okt1:<8}  {okt2:<8}  {okt3:<8}
+{okt0:08b}  {okt1:08b}  {okt2:08b}  {okt3:08b}
+'''
+
+print(network_template.format(  ip0 = int(ip.split('.')[0]),
+                                ip1 = int(ip.split('.')[1]),
+                                ip2 = int(ip.split('.')[2]),
+                                ip3 = int(ip.split('.')[3])))
+print(mask_template.format( mask = mask,
+                            okt0 = int(mask_bin[0:8], 2),
+                            okt1 = int(mask_bin[8:16], 2),
+                            okt2 = int(mask_bin[16:24], 2),
+                            okt3 = int(mask_bin[24:32], 2)))
