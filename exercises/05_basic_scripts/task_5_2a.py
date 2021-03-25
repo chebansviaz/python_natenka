e7163174c87b951f7a952433bbe5c4025c92057c
Cheban Andrey Mihailovich <acheban@mtb.ua>
2021-03-25 13:04:07 +0200

    5.2a done

--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -41,3 +41,40 @@ bin_ip = "00001010000000010000000111000011"
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+network_input = input('Введите IP-сети в формате: 10.1.1.0/24 : ')
+
+ip, mask = network_input.split("/")
+
+network_template = '''
+Network:
+{ip0:<8}  {ip1:<8}  {ip2:<8}  {ip3:<8}
+{ip0:08b}  {ip1:08b}  {ip2:08b}  {ip3:08b}
+'''
+
+mask_bin = "1"*int(mask) + "0"*(32-int(mask))
+
+mask_template = '''
+Mask:
+/{mask}
+{okt0:<8}  {okt1:<8}  {okt2:<8}  {okt3:<8}
+{okt0:08b}  {okt1:08b}  {okt2:08b}  {okt3:08b}
+'''
+
+ip_bin = "{:08b}{:08b}{:08b}{:08b}".format( int(ip.split('.')[0]), int(ip.split('.')[1]), int(ip.split('.')[2]), int(ip.split('.')[3]))
+
+#ip_template = '''{ip0:08b}{ip1:08b}{ip2:08b}{ip3:08b}'''
+
+#ip_bin = ip_template.format( ip0 = int(ip.split('.')[0]), ip1 = int(ip.split('.')[1]), ip2 = int(ip.split('.')[2]), ip3 = int(ip.split('.')[3]))
+
+net_bin = "{}{}".format(ip_bin[0:int(mask)], "0"*len(ip_bin[int(mask):32]))
+
+print(network_template.format(  ip0 = int(net_bin[0:8], 2),
+                                ip1 = int(net_bin[8:16], 2),
+                                ip2 = int(net_bin[16:24], 2),
+                                ip3 = int(net_bin[24:32], 2)))
+
+print(mask_template.format( mask = mask,
+                            okt0 = int(mask_bin[0:8], 2),
+                            okt1 = int(mask_bin[8:16], 2),
+                            okt2 = int(mask_bin[16:24], 2),
+                            okt3 = int(mask_bin[24:32], 2))) 
