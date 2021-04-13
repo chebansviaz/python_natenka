b23704f3966df7617a47f7e0f6f4099934561a98
Cheban Andrey Mihailovich <acheban@mtb.ua>
2021-04-13 16:25:33 +0300

    12.1 done

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,18 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+
+def ping_ip_addresses(list_ip_adress):
+    alive = []
+    unreachable = []
+    for ip in list_ip_adress:
+        reply = subprocess.run(['ping', '-c', '3', '-n', ip])
+        if reply.returncode == 0:
+            alive.append(ip)
+        else:
+            unreachable.append(ip)
+
+    return (alive, unreachable )
+
+print(ping_ip_addresses(["8.8.8.8", "9.9.9.9", "192.168.7.1", "192.168.7.2"]))
