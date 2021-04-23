c46f19eeb60d885949a5588b91494392b4164a5a
Cheban Andrey Mihailovich <acheban@mtb.ua>
2021-04-23 22:11:52 +0300

    15.2a done

--- exercises/15_module_re/task_15_2a.py
+++ exercises/15_module_re/task_15_2a.py
@@ -34,3 +34,33 @@ data = [
     ("R2", "15.2(2)T1", "Cisco 2911"),
     ("SW1", "12.2(55)SE9", "Cisco WS-C2960-8TC-L"),
 ]
+#import re
+#from pprint import pprint
+#def convert_to_dict(list_headers, list_tuples):
+#    result = []
+#    regex = (r'(?P<HOSTNAME>(?:R|SW)\d+)|'
+#             r'(?P<IOS>\S+\.\S+)|'
+#             r'(?P<PLATFORM>\S+ \S+)')
+#    for line in data:
+#        dict_line = {}
+#        for value in line:
+#            patern = re.match(regex, value)
+#            if patern.group('HOSTNAME'):
+#                dict_line[headers[0]] = patern.group('HOSTNAME')
+#            elif patern.group('IOS'):
+#                dict_line[headers[1]] = patern.group('IOS')
+#            elif patern.group('PLATFORM'):
+#                dict_line[headers[2]] = patern.group('PLATFORM')
+#        result.append(dict_line)
+#    return result
+
+#def convert_to_dict(list_headers, list_tuples):
+#    for line in list_tuples:
+#        print(dict(zip(list_headers, line)))
+#    result = [dict(zip(list_headers, line)) for line in list_tuples]
+#    return result
+
+convert_to_dict = lambda list_headers, list_tuples: [dict(zip(list_headers, line)) for line in list_tuples]
+
+if __name__ == "__main__":
+    pprint(convert_to_dict(headers, data))
